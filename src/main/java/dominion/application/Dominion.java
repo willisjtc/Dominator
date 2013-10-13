f06f315fb73b6f45de67dda30e1b9d92eedd0865
Jonathan Willis <quicksilversly@gmail.com>
2013-10-12 21:38:39 -0600

    making some good progress

--- src/main/java/dominion/application/Dominion.java
+++ src/main/java/dominion/application/Dominion.java
@@ -1,7 +1,5 @@
 package dominion.application;
 
-import java.lang.reflect.Field;
-
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +9,6 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import dominion.database.dao.PlayerDAO;
-import dominion.game.user.Player;
 
 public class Dominion extends Application {
 	@Override
@@ -20,7 +16,7 @@ public class Dominion extends Application {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));		
 			
-			Scene scene = new Scene(root, 600, 275);
+			Scene scene = new Scene(root, 800, 575);
 			scene.getStylesheets().add(Dominion.class.getResource("login.css").toExternalForm());
 			primaryStage.setTitle("Dominion Client");
 			primaryStage.setScene(scene);
@@ -28,21 +24,6 @@ public class Dominion extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		PlayerDAO playerDao = new PlayerDAO();
-		Player player = playerDao.getPlayerById(1);
-		Object playerRef = player;
-		Field[] fields = playerRef.getClass().getFields();
-		
-		for (int i = 0; i < fields.length; i++) {
-			System.out.println("field: " + fields[i]);
-		}
-		for (int i = 0; i < playerRef.getClass().getMethods().length; i++) {
-			System.out.println("method: " + playerRef.getClass().getMethods()[i]);
-		}
-		System.out.println("player: " + player.getName() + " " + player.getUsername() + " " + player.getPassword());
-//		BasicDAO basicDao = new BasicDAO();
-//		String name = basicDao.getName();
-//		System.out.println("name: " + name);
 	}
 	
 	@FXML public void signInReleased(MouseEvent evt) {
---
+++ src/main/java/dominion/database/utils/DatabaseProperties.java
@@ -0,0 +1,27 @@
+package dominion.database.utils;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class DatabaseProperties {
+	private static Map<String, String> properties = initMap();
+	
+	public static String getProperty(String property) {
+		System.out.println("getProperty(String" + property + ")");
+		return properties.get(property);
+	}
+	
+	private static Map<String, String> initMap() {
+		System.out.println("initMap()");
+		Map<String, String> properties = new HashMap<String, String>();
+		ResourceBundle rb = ResourceBundle.getBundle("dominiondatabase");
+		Enumeration<String> keys = rb.getKeys();
+		while (keys.hasMoreElements()) {
+			String key = keys.nextElement();
+			properties.put(key, rb.getString(key));
+		}
+		return properties;
+	}
+}
