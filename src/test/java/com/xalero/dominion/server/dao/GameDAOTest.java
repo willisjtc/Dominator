e68207195aeb4d376a46b0ebdfd34d5526cbab38
Jonathan Willis <quicksilversly@gmail.com>
2014-03-09 20:39:14 -0600

    no message

--- src/test/java/com/xalero/dominion/server/dao/GameDAOTest.java
+++ src/test/java/com/xalero/dominion/server/dao/GameDAOTest.java
@@ -1,23 +1,26 @@
 package com.xalero.dominion.server.dao;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+//
+//public class GameDAOTest extends DominionTestBase {
+//	
+//	public GameDAOTest() {
+//		
+//	}
+//	
+//	public GameDAOTest(String dataSet, String driver, String connectionUrl,
+//			String dbUsername, String dbPassword) throws DataSetException,
+//			FileNotFoundException, IOException {
+//        super("game_dao_test_fixture.xml",
+//          	  TestProperties.getProperty("db.driver"),
+//          	  TestProperties.getProperty("db.connection.url"),
+//          	  TestProperties.getProperty("db.username"),
+//          	  TestProperties.getProperty("db.password"));
+//
+//	}
+//
+//	@Test
+//	public void test() {
+//		assertTrue(true);
+//	}
+//}
 
-import org.dbunit.dataset.DataSetException;
-import org.junit.Test;
-
-import com.xalero.dominion.utils.DominionTestBase;
-
-public class GameDAOTest extends DominionTestBase {
-
-	public GameDAOTest(String dataSet, String driver, String connectionUrl,
-			String dbUsername, String dbPassword) throws DataSetException,
-			FileNotFoundException, IOException {
-		super(dataSet, driver, connectionUrl, dbUsername, dbPassword);
-	}
-
-	@Test
-	public void test() {
-		assertTrue(true);
-	}
-}
